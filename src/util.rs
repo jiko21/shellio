cecb89978da7310a64ad82a70b05c9a51c786403
daikikojima <daikikojima0531@icloud.com>
2021-09-18 16:57:30 +0900

    add util for execute and read command

---
+++ src/util.rs
@@ -0,0 +1,28 @@
+pub mod util {
+    use std::process::Command;
+    use std::str::from_utf8;
+    pub fn execute_command(command: &str) -> String {
+        let output = if cfg!(target_os = "windows") {
+            Command::new("cmd")
+                .args(&["/C", command])
+                .output()
+                .expect(&*format!("failed to execute command: {}", command))
+        } else {
+            Command::new("sh")
+                .arg("-c")
+                .arg(command)
+                .output()
+                .expect(&*format!("failed to execute command: {}", command))
+        };
+        from_utf8(&*output.stdout).unwrap().to_string()
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+        #[test]
+        fn test_execute_command() {
+            assert_eq!(execute_command("echo test"), "test\r\n");
+        }
+    }
+}
